94d37a8ef9ac712d381f213baa8473b7d26cff70
jfbueno <buenojeferson@outlook.com>
2022-04-06 23:23:24 -0300

    🎨 Melhoria em nomes e remoção de código não usado

--- Program.cs
+++ Program.cs
@@ -1,23 +1,30 @@
 ﻿using System.Text.RegularExpressions;
+using static System.Console;
 
-const string whiteListFilePath = "./whitelist.txt";
+const string caminhoArquivoWhitelist = "./whitelist.txt";
 var regexCpf = new Regex(@"^((\d{3}\.){2}\d{3}\-\d{2}|\d{11})$");
 
-var entrada = args?[1] ?? throw new Exception("Entrada deve ser informada");
+if (args is not { Length: 2 })
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    WriteLine("Este programa precisa de 2 entradas para ser executado.");
+    WriteLine("\t1. Tipo de validação ('regex' ou 'whitelist')");
+    WriteLine("\t2. Dado para ser validado");
+    return;
+}
 
-var entradaVálida = args?[0] switch
+var entrada = args[1];
+var entradaVálida = args[0] switch
 {
     "regex" => ValidarComRegex(entrada),
     "whitelist" => ValidarComWhitelist(entrada),
     _ => throw new Exception($"Tipo de validação deve ser 'regex' ou 'whitelist'")
 };
 
-Console.WriteLine($"{entrada} é um CPF {(entradaVálida ? "válido" : "inválido")}");
+WriteLine($"'{entrada}' é um CPF {(entradaVálida ? "válido" : "inválido")}");
 
 bool ValidarComRegex(string entrada)
     => regexCpf.IsMatch(entrada);
 
 bool ValidarComWhitelist(string entrada)
-    => File.ReadAllLines(whiteListFilePath).Contains(entrada);
-
-enum TipoValidacao { Regex, Whitelist }
+    => File.ReadAllLines(caminhoArquivoWhitelist).Contains(entrada);
